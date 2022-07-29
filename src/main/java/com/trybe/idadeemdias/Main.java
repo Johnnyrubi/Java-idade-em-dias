5487e71474a214ee32e6640f6152d820d870cc00
johnny <johnnyrubischreiner@gmail.com>
2022-07-28 22:09:08 -0300

    exercicio concluido

--- src/main/java/com/trybe/idadeemdias/Main.java
+++ src/main/java/com/trybe/idadeemdias/Main.java
@@ -4,10 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+  public static void main(String[] args) {
+    Pessoa qualquer = new Pessoa();
 
-		
-	}
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Qual seu nome?");
+    String nome = scanner.next();
+    System.out.println("Qual é sua idade em anos, meses e dias?");
+    System.out.println("anos:");
+    String ano = scanner.next();
+    System.out.println("meses:");
+    String meses = scanner.next();
+    System.out.println("dias:");
+    String dias = scanner.next();
+
+    int a = Integer.parseInt(ano);
+    int b = Integer.parseInt(meses);
+    int c = Integer.parseInt(dias);
+
+    Long result = qualquer.calcularIdadeEmDias(a, b, c);
+    System.out.println("Olá " + nome + ", sua idade em dias é " + result);
+    scanner.close();
+  }
 
 }
