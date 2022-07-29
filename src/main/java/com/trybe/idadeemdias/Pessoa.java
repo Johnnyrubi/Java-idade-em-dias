d98f146d8644868f9f1c34d4718940f4fe900e3d
johnny <johnnyrubischreiner@gmail.com>
2022-07-28 22:18:46 -0300

    exercicio concluido

--- src/main/java/com/trybe/idadeemdias/Pessoa.java
+++ src/main/java/com/trybe/idadeemdias/Pessoa.java
@@ -2,7 +2,7 @@ package com.trybe.idadeemdias;
 
 public class Pessoa {
   /**
-   * class Pessoa
+   * class Pessoa.
    */
   public Long calcularIdadeEmDias(int anos, int meses, int dias) {
     Long idade = (long) ((anos * 365) + (meses * 30) + dias);
